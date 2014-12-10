95688c2000c1dfc01729c1b687aeae38cb5e5cae
Grzegorz Milka <grzegorzmilka@gmail.com>
2014-12-10 09:50:06 +0100

    Add basic GUI

--- src/main/java/me/gregorias/xmllibrary/Main.java
+++ src/main/java/me/gregorias/xmllibrary/Main.java
@@ -1,20 +1,31 @@
 package me.gregorias.xmllibrary;
 
+import me.gregorias.xmllibrary.interfaces.gui.MainApplication;
+import me.gregorias.xmllibrary.interfaces.rest.RESTApplication;
 import me.gregorias.xmllibrary.library.jaxb.Library;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import java.net.URI;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Main {
   private static Path PATH_TO_LIBRARY_XML =
     FileSystems.getDefault().getPath("resources/config/library.xml");
+
   public static void main(String[] args) throws JAXBException {
     JAXBContext jc = JAXBContext.newInstance(Library.class);
     Unmarshaller unmarshaller = jc.createUnmarshaller();
     Library library = (Library) unmarshaller.unmarshal(PATH_TO_LIBRARY_XML.toFile());
-    System.out.println(library.getPositions());
+
+    MainApplication.main(args);
+    final URI baseURI = URI.create(String.format("http://%s:%s/", "localhost", "8080"));
+    Lock lock = new ReentrantReadWriteLock().readLock();
+    RESTApplication rest = new RESTApplication(library, lock, baseURI);
+    rest.run();
   }
 }
--- src/main/java/me/gregorias/xmllibrary/interfaces/gui/MainApplication.java
+++ src/main/java/me/gregorias/xmllibrary/interfaces/gui/MainApplication.java
@@ -3,9 +3,16 @@ package me.gregorias.xmllibrary.interfaces.gui;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -15,14 +22,11 @@ public class MainApplication extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    Button btn = new Button();
-    btn.setText("Say 'Hello World'");
-    btn.setOnAction(new HelloWorldEventHandler());
+    BorderPane borderPane = new BorderPane();
 
-    StackPane root = new StackPane();
-    root.getChildren().add(btn);
+    borderPane.getChildren().add(createTopPane());
 
-    Scene scene = new Scene(root, 300, 250);
+    Scene scene = new Scene(borderPane, 300, 250);
 
     primaryStage.setTitle("Hello World!");
     primaryStage.setScene(scene);
@@ -38,5 +42,21 @@ public class MainApplication extends Application {
     public void handle(ActionEvent event) {
       System.out.println("Hello World!");
     }
+  }
+
+  private HBox createTopPane() {
+    HBox hbox = new HBox();
+    hbox.setPadding(new Insets(15, 12, 15, 12));
+    hbox.setSpacing(10);
+    hbox.setStyle("-fx-background-color: #336699;");
+    Text scenetitle = new Text("XMLLibrary");
+    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+    Button loginButton = new Button("login");
+    loginButton.setAlignment(Pos.CENTER_RIGHT);
+    Button registerButton = new Button("register");
+    registerButton.setAlignment(Pos.CENTER_RIGHT);
+    hbox.getChildren().addAll(scenetitle, loginButton, registerButton);
+    hbox.setHgrow(scenetitle, Priority.ALWAYS);
+    return hbox;
   }
 }
